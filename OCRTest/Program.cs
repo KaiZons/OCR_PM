ed837367f883c5a264aaad932e5f9b161e580c43
KaiZons <2516361678@qq.com>
2019-04-17 16:13:07 +0800

    发个非官方个

--- OCRTest/Program.cs
+++ OCRTest/Program.cs
@@ -17,7 +17,7 @@ namespace OCRTest
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RecognizeWordsOnPic());
+            Application.Run(new RecognizeWordsOnPic());      
         }
     }
 }
